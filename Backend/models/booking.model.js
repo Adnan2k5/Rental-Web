38631d6ac58b4f6aa14bb4704aacd4aa1c0fadbd
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-03-31 16:48:07 +0530

    Add booking model with schema for user bookings, including payment and status fields

---
+++ Backend/models/booking.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        item: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Item",
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 1,
+        },
+        bookingDate: {
+            type: Date,
+            default: Date.now,
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ["pending", "confirmed", "canceled"],
+            default: "pending",
+        },
+        paymentStatus: {
+            type: String,
+            enum: ["pending", "completed", "failed"],
+            default: "pending",
+        },
+        paymentMethod: {
+            type: String,
+            enum: ["credit_card", "paypal", "bank_transfer"],
+            default: "credit_card",
+        },
+        paymentDetails: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: "Payment",
+        },
+    },
+    { timestamps: true }
+);
+
+export const Booking = mongoose.model("Booking", bookingSchema);
