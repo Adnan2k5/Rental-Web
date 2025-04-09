da1e4d9e96bc0b8f875dde4f53e58924a6777890
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-04-10 00:59:29 +0530

    Implement removeItemFromCart functionality in user controller

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -116,3 +116,40 @@ export const addItemToCart = asyncHandler(async (req, res) => {
 
     return res.status(200).json(new ApiResponse(200, user.cart, "Item added to cart successfully"));
 });
+
+export const removeItemFromCart = asyncHandler(async (req, res) => {
+    const { itemId, del } = req.body;
+
+    if (!itemId) {
+        throw new ApiError(400, "Item ID is required");
+    }
+
+    const cart = await Cart.findOne({ user: req.user._id });
+
+    if (!cart) {
+        throw new ApiError(404, "Cart not found");
+    }
+
+    const itemIndex = cart.items.findIndex(item => item.item.toString() === itemId);
+
+    if (itemIndex > -1) {
+
+        if(del === "true") {
+            cart.items.splice(itemIndex, 1);
+            return res.status(200).json(new ApiResponse(200, user.cart, "Item removed from cart successfully"));
+        }
+
+        cart.items[itemIndex].quantity -= 1;
+
+        if (cart.items[itemIndex].quantity <= 0) {
+            cart.items.splice(itemIndex, 1);
+        }
+        
+    } else {
+        throw new ApiError(404, "Item not found in cart");
+    }
+
+    await cart.save();
+
+    return res.status(200).json(new ApiResponse(200, user.cart, "Item removed from cart successfully"));
+});
