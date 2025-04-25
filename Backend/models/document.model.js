3793b3e6c53cd6bcb6e515d46c54b2dbac0aa8e3
Md Saquib Jawed <saquibjawed4444@gmail.com>
2025-04-25 21:29:20 +0530

    Add Document model with schema definition for document management

---
+++ Backend/models/document.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const documentSchema = new mongoose.Schema(
+    {
+        documentUrl: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        imageUrl: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        country: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export const Document = mongoose.model("Document", documentSchema);
